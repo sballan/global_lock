73178a41f43f7b07a5e1f19a094ac6bf3f08468b
Samuel Ballan <sgb4622@gmail.com>
2021-02-05 15:04:35 -0500

    dumb pattern to get include/extend working like expected, using singleton in mixin

--- lib/global_lock/lockable.rb
+++ lib/global_lock/lockable.rb
@@ -1,29 +1,37 @@
 module GlobalLock
   module Lockable
+    def self.included(other_mod)
+      # This pattern lets us have instance _and_ class methods in this module
+      other_mod.extend ClassMethods
+    end
+
     def lock_id
       send(self.class.lock_id_name)
     end
 
     def lock(opts={})
-      Lock.lock(lock_id, opts)
+    GlobalLock.singleton.lock(lock_id, opts)
     end
 
     def unlock(key)
-      Lock.unlock(lock_id, key)
+      GlobalLock.singleton.unlock(lock_id, key)
     end
 
     def with_lock(existing_key=nil, opts={}, &block)
-      Lock.with_lock(lock_id, existing_key, opts, &block)
+    GlobalLock.singleton.with_lock(lock_id, existing_key, opts, &block)
     end
 
-    def self.lock_id_name
-      @lock_id_name || :id
-    end
+    # This pattern lets us have instance _and_ class methods in this module
+    module ClassMethods
+      def lock_id_name
+        @lock_id_name || :id
+      end
 
-    def self.set_lock_id_name(lock_id_name)
-      raise "Lock name must be symbol" unless lock_id_name.is_a? Symbol
+      def set_lock_id_name(lock_id_name)
+        raise "Lock name must be symbol" unless lock_id_name.is_a? Symbol
 
-      @lock_id_name = lock_id_name
+        @lock_id_name = lock_id_name
+      end
     end
   end
 end
